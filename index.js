93b155b2dec3fd704811184db6bc26c8938d1e19
Ubuntu <ubuntu@ip-172-31-33-27.eu-west-1.compute.internal>
2023-10-18 15:21:39 +0000

    -m Change

---
+++ index.js
@@ -0,0 +1,6 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+app.get('/',(req, res) => res.send('Hello there!'));
+app.listen(PORT, () => console.log(`Server listening in port ${PORT}`))
+
